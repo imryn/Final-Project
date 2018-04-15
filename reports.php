ebcdec8631162980cfd563949dd0bf5b4fe3c37c
imryn <noyimry@gmail.com>
2018-04-15 21:21:35 +0300

    fix the problem alergic was apper bofore choose option

--- reports.php
+++ reports.php
@@ -22,7 +22,7 @@
 
 </head>
 
-<body onload="getReports()">
+    <body>
        <header>
             <templateHtml src="picture-container/picture-container.html"></templateHtml>
             <?php include "nav-menu/nav-menu-container.php" ?>
