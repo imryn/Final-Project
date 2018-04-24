0697e707a3078eedee7d0c13e5d7817e23406f69
imryn <noyimry@gmail.com>
2018-04-24 14:31:14 +0300

    fix more for login button

--- nav-menu/nav-menu.php
+++ nav-menu/nav-menu.php
@@ -21,8 +21,8 @@
                 Login 
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="login_page.php?usertype=crew"> Login for crew </a>
-                <a class="dropdown-item" href="login_page.php?usertype=parent"> Login for parents </a>
+                <a class="dropdown-item" href="login_page.php?usertype=crew"> Login as crew </a>
+                <a class="dropdown-item" href="login_page.php?usertype=parent"> Login as parents </a>
               </div>
             </li>
             <li class="links"><a href="parent-registration.php"> registration </a></li>
