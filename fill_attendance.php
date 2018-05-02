4e23e13bde2458a4653348a52f614bec77c987b9
karinhp1 <37689513+karinhp1@users.noreply.github.com>
2018-05-02 19:50:33 +0300

    K

--- fill_attendance.php
+++ fill_attendance.php
@@ -111,7 +111,7 @@
                                     else{echo '';}; ?>
                                     type="checkbox" data-toggle="toggle">
                                 </td>
-                                <td> <input type="button" checked value="Send" class="send-button" onClick="sendEmail()"/> </td>
+                                <td> <input type="button" checked value="Send" class="send-button btn btn-warning" onClick="sendEmail()"/> </td>
                                 <td style="display:none;" class='parentId'> <?php echo $row['parentId']; ?> </td>
                                 <td style="display:none;" class='kindergartenid'> <?php echo $row['kindergartenid']; ?> </td>                             
                                 <?php $counter++; ?>
