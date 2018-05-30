999a12566841311038c89d12b9c1f589515f8b2a
Daniel Ben-Moshe <danielbenmoshe4@gmail.com>
2018-05-31 00:07:16 +0300

    weather in one line

--- weather/api.php
+++ weather/api.php
@@ -1,52 +1,13 @@
 <div>
     <table id="weather-table" class="table-responsive">
+
         <tr>
-            <th>Today</th>
-            <th>Tomorrow</th>
-            <th>Day 3</th>
-            <th>Day 4</th>
-        </tr>
+            <td>Today:</td> <td class="today_temp"></td> <td class="today_icon"></td> <td class="today_desc"></td>
         <tr>
-            <td class="today_temp">
 
-            </td>
-            <td class="tomorrow_temp">
-
-            </td>
-            <td class="day3_temp">
-
-            </td>
-            <td class="day4_temp">
-
-            </td>
-        </tr>
-        <tr>
-            <td class="today_icon">
-
-            </td>
-            <td class="tomorrow_icon">
-
-            </td>
-            <td class="day3_icon">
-
-            </td>
-            <td class="day4_icon">
-
-            </td>
-        </tr>
-        <tr>
-            <td class="today_desc">
-
-            </td>
-            <td class="tomorrow_desc">
-
-            </td>
-            <td class="day3_desc">
-
-            </td>
-            <td class="day4_desc">
-
-            </td>
-        </tr>
+        <!-- <tr>
+            <td>Tomorrow:</td> <td class="tomorrow_temp"></td> <td class="tomorrow_icon"></td> <td class="tomorrow_desc"></td>
+        <tr> -->
+        
     </table>
 </div>
